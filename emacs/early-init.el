52b75ed18286a89b2a242b652aa7896066e6fa7d
James Yoo <jyoo980@gmail.com>
2026-07-03 14:12:00 -0700

    Initial commit

---
+++ emacs/early-init.el
@@ -0,0 +1,167 @@
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+    (expand-file-name
+      "straight/repos/straight.el/bootstrap.el"
+      (or (bound-and-true-p straight-base-dir)
+        user-emacs-directory)))
+    (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+       'silent 'inhibit-cookies)
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+(setq use-package-always-defer t)
+
+(use-package gcmh
+  :demand
+  :config
+  (gcmh-mode 1))
+
+
+(use-package emacs
+  :init
+  (setq initial-scratch-message nil)
+  (defun display-startup-echo-area-message ()
+    (message ""))
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  (set-charset-priority 'unicode)
+  (setq locale-coding-system 'utf-8
+        coding-system-for-read 'utf-8
+        coding-system-for-write 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width 2)
+  (set-face-attribute 'default nil
+    :font "PragmataPro Mono Liga"
+    :height 160))
+
+
+(use-package evil
+  :demand ; No lazy loading
+  :config
+  (evil-mode 1))
+
+(use-package nerd-icons)
+
+(use-package doom-themes
+  :demand
+  :config
+  (load-theme 'doom-tomorrow-day t))
+
+(use-package which-key
+  :demand
+  :init
+  (setq which-key-idle-delay 0.5)
+  :config
+  (which-key-mode))
+
+(use-package ivy
+  :config
+  (ivy-mode))
+
+(use-package general
+  :demand
+  :config
+  (general-evil-setup)
+
+  (general-create-definer leader-keys
+    :states '(normal insert visual emacs)
+    :keymaps 'override
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (leader-keys
+    "x" '(execute-extended-command :which-key "execute command")
+    "r" '(restart-emacs :which-key "restart emacs")
+    "i" '((lambda () (interactive) (find-file user-init-file)) :which-key "open init file")
+
+    ;; Buffer
+    "b" '(:ignore t :which-key "buffer")
+    ;; Don't show an error because SPC b ESC is undefined, just abort
+    "b <escape>" '(keyboard-escape-quit :which-key t)
+    "bd"  'kill-current-buffer
+  ))
+
+
+(use-package magit
+  :general
+  (leader-keys
+    "g" '(:ignore t :which-key "git")
+    "g <escape>" '(keyboard-escape-quit :which-key t)
+    "g g" '(magit-status :which-key "status")
+    "g l" '(magit-log :which-key "log"))
+  (general-nmap
+    "<escape>" #'transient-quit-one))
+
+(use-package diff-hl
+  :init
+  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  :config
+  (global-diff-hl-mode))
+
+(use-package vterm-toggle
+  :general
+  (leader-keys
+    "'" '(vterm-toggle :which-key "terminal")))
+
+(with-eval-after-load 'vterm
+  (defun my/vterm-clear-all ()
+    "⌘K semantics: clear screen and scrollback, like iTerm2."
+    (interactive)
+    (vterm-clear-scrollback)
+    (vterm-clear))          ; sends C-l so the shell redraws its prompt
+
+  (defun my/vterm-isearch ()
+    "⌘F semantics: search the scrollback."
+    (interactive)
+    (vterm-copy-mode 1)     ; freeze the buffer so point can move
+    (isearch-backward))
+
+  (define-key vterm-mode-map (kbd "s-k") #'my/vterm-clear-all)
+  (define-key vterm-mode-map (kbd "s-v") #'vterm-yank)       ; paste via bracketed paste
+  (define-key vterm-mode-map (kbd "s-c") #'kill-ring-save)   ; copy region
+  (define-key vterm-mode-map (kbd "s-f") #'my/vterm-isearch)
+  (define-key vterm-mode-map (kbd "s-t") #'vterm))
+
+(defun my/startup-vterm-split ()
+  "Split frame right, open vterm in the new split"
+  (let ((original (selected-window)))
+    (select-window (split-window-right))
+    (vterm)
+    (select-window original))
+  (let ((original (selected-window)))
+    (select-window (split-window-vertically))
+    (find-file "~/.org/misc.org")
+    (select-window original)))
+
+(add-hook 'emacs-startup-hook #'my/startup-vterm-split)
+
+; Org-mode stuff
+(add-hook 'org-mode-hook                                                                      
+          (lambda ()                                                                          
+        (define-key evil-normal-state-map (kbd "TAB") 'org-cycle))) 
+
+(setq org-todo-keywords
+   '((sequence "TODO" "IN-PROGRESS" "BLOCKED" "DONE")))
+
+(setq org-todo-keyword-faces
+      '(("TODO" . (:foreground "black" :weight bold))
+        ("IN-PROGRESS" . "orange")
+        ("BLOCKED" . "red")
+        ("DONE" . "green")))
